2e501314b58cceefa849c81040e64429f103d35d
Park HyeonSeok <157240336+autumn63@users.noreply.github.com>
2025-11-28 23:05:55 +0900

    Delete video_hyenseok directory
    main에 잘못 올렸습니다. 죄송합니다

--- video_hyenseok/moviepy.py
+++
@@ -1,8 +0,0 @@
-#moviepy를 활용. 이미지 여러개를 이어서 영상 생성.
-#pip install moviepy 필요하다.
-
-from moviepy.editor import *
-
-clip = ImagesequenceClip(['frame1.png', 'frame2.png', 'frame3.png'], fps=1)
-
-clip.ipython_display(width=360)
