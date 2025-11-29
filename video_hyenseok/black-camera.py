b1cc2e9d960a3e4b2eefb21bd17e39540699b84c
Park HyeonSeok <hyenseok44@gmail.com>
2025-11-29 20:04:21 +0900

    video1 0.1.1
    color-camera.py 카메라를 열어서 원본영상과 검정색부분을 노란색으로 바꾼 영상 출력.
    hsv를 이용해서 색상, 채도, 밝기 조절.
     lower_black = np.array([0, 0, 0])
        upper_black = np.array([180, 255, 60]) 검정색이라고 판단되는 부분 정의.
    colored.avi로 저장됨
    
    black-camer.py
    원본영상의 흑백영상만 출력. black.avi로 저장됨.

--- video_hyenseok/black-camera.py
+++ video_hyenseok/black-camera.py
@@ -21,7 +21,7 @@ capture = cv2.VideoCapture(0) #카메라 열기
 #cv2.VideoCapture(index) index = 0 노트북 내장카메라  index = 1~n 외장카메라
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID') #코덱 설정
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480)) #output.avi로 저장됨, 코덱, fps, 프레임크기
+out = cv2.VideoWriter('black.avi', fourcc, 20.0, (640,480)) #output.avi로 저장됨, 코덱, fps, 프레임크기
 
 if not capture.isOpened():
     print("Camera open failed!") # 열리지 않았을때 출력
@@ -30,8 +30,9 @@ if not capture.isOpened():
 while True:
 
     ret , frame = capture.read()
-    out.write(frame) #영상 저장
-    cv2.imshow("frame", frame) #카메라 영상 출력
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #흑백영상으로 전환.
+    out.write(frame) #영상 저장 
+    cv2.imshow("gray", gray) #흑백 영상 출력 원래영상,
 
     if cv2.waitKey(1) == 27: #esc키 누르면 종료
         break
